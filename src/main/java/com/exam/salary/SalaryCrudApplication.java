eade6a8941d5c0772d086fa77b97bbcbabbcbc4d
Yonathan Gonzalez <y.j.gonz.n@gmail.com>
2021-03-14 12:06:53 -0500

    first commit

---
+++ src/main/java/com/exam/salary/SalaryCrudApplication.java
@@ -0,0 +1,13 @@
+package com.exam.salary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryCrudApplication.class, args);
+    }
+
+}
